684d774226ad5961b506afbaacc93e81f63fbe82
Fausto Armando Muñoz Chacon <faustomunoz2004@gmail.com>
2025-03-03 10:33:41 -0600

    Renderizado switch case

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  let numero = 8;
 
   return (
     <>
-      <h1 className='clase_morada'>Uso de className</h1>
+      <h1>Renderizado switch case</h1>
+      {(() => {
+        switch (numero) {
+          case 13:
+            return <div>Numero es 13</div>;
+          case 12:
+            return <div>Numero es 12</div>;
+          default:
+            return <div>El numero no es ni 13 ni 12</div>;
+        }
+      })()}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
