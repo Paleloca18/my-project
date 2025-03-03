a912f85fd3f2623cf6c4b36e2e0e6f3c57fdb6ba
Fausto Armando Muñoz Chacon <faustomunoz2004@gmail.com>
2025-03-03 10:33:02 -0600

    Crea un componente de clase
    Estructura de un componente de clase que ya no se usa, sin embargo es necesario conocerlo para paginas hechas en el pasado que usan este tipo de componentes

---
+++ src/components/ComponenteDeClase.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ComponenteDeClase extends React.Component{
+    render(){
+        return(
+            <>
+                <h1>Soy un componente de clase!!</h1>
+            </>
+        )
+    }
+}
